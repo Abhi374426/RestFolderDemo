925ffb0092216c364ade9c2fa94dfd230ea61b5d
Abhishek jangid <abhi963696@gmail.com>
2025-03-26 10:57:56 +0530

    Add another method

--- first.java
+++ first.java
@@ -4,5 +4,8 @@ public Class first{
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+    public static void run(){
+        System.out.println("Hello"); 
+    }
 
 }
