8de73a721a1d61590637316e4e4570a332587d96
Abhishek jangid <abhi963696@gmail.com>
2025-03-27 14:24:50 +0530

    nfkvnkvm

--- first.java
+++ first.java
@@ -2,11 +2,7 @@ public Class first{
 
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello gi");
-=======
-        System.out.println("Hello G");
->>>>>>> about
     }
     public static void run(){
       System.out.println("this is sout of the  show data for change  vff is branch");
--- micro.java
+++ micro.java
@@ -1,7 +1,3 @@
 public class micro {
-<<<<<<< HEAD
    kk
-=======
-    hi
->>>>>>> ca4896d8f49e96f659597225092bd4f986a89d29
 }
