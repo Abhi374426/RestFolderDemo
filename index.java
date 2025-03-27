0c28861bc395673064c7512aa3e2eba8a62f4866
Abhi123-cloud-123 <mohen9543@gmail.com>
2025-03-27 17:02:16 +0530

    Update index.java
    add this nrnsacdncn

--- index.java
+++ index.java
@@ -2,7 +2,7 @@ public class index {
 <<<<<<< HEAD
    sysout("this is chagnge by local system");
 =======
-   sysout("this is chagnge by Abhi cloud");
+   sysout("this is chagnge by Abhi cloud   ndnvvndjvd");
    000
 //>>>>>>> 70dbd87abe8951a003eee9ac3507294d6aa58ba4
     
