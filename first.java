11e0f35af8df181fd4e47bbb920a9fba141300a8
Abhishek jangid <abhi963696@gmail.com>
2025-03-27 11:45:04 +0530

    this is pull message

--- first.java
+++ first.java
@@ -6,6 +6,7 @@ public Class first{
     }
     public static void run(){
       System.out.println("this is sout of the  show data for change  vff is branch");
+      this is pull
          System.out.println("this is sout of the  show data for change  sff is branch");
     }
 
