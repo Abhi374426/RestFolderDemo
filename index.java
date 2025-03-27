3f3b473b78f8153ae185d02745ae78053cac801d
Abhishek jangid <abhi963696@gmail.com>
2025-03-27 17:38:56 +0530

    jfjnf

--- index.java
+++ index.java
@@ -3,7 +3,7 @@ public class index {
    sysout("this is chagnge by local system");
 =======
    sysout("this is chagnge by Abhi cloud   ndnvvndjvd");
-   000ok done
+   000ok done bdbf
 //>>>>>>> 70dbd87abe8951a003eee9ac3507294d6aa58ba4
     
 }
